b5c9d7a4982c26dc370bf0ac8ddf80d8bc61706d
AcerAspire3 <kalilinux@KaliLinux.KaliLinux>
2026-04-16 07:05:17 +0500

    Thirth Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { store } from "@/app/store";
-import { router } from "@/router";
-import "@/index.css";
+
+import AppRouter from "./Router/index.jsx";
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      staleTime: 60 * 1000,
+      retry: 1,
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 5,
     },
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
+  </StrictMode>,
 );
